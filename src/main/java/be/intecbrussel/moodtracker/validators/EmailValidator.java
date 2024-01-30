0c446810326a992e100a78e4af0816883407a766
zoewijnendaele <zoewijnendaele@gmail.com>
2024-01-30 11:02:43 +0100

    [NEW] EmailValidator

---
+++ src/main/java/be/intecbrussel/moodtracker/validators/EmailValidator.java
@@ -0,0 +1,20 @@
+package be.intecbrussel.moodtracker.validators;
+
+import jakarta.validation.ConstraintValidator;
+import jakarta.validation.ConstraintValidatorContext;
+
+import java.util.regex.Pattern;
+
+public class EmailValidator implements ConstraintValidator<ValidEmail, String> {
+
+    private static final String EMAIL_PATTERN = "^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\\\\.[A-Z|a-z]{2,}$";
+
+    @Override
+    public void initialize(ValidEmail constraintAnnotation) { }
+
+    @Override
+    public boolean isValid(String email, ConstraintValidatorContext context) {
+        return email != null && Pattern.compile(EMAIL_PATTERN).matcher(email).matches();
+    }
+
+}
