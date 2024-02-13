d33988c37cd76d207b5070f9085a0f5df3fcc469
zoe <zoewijnendaele@gmail.com>
2024-02-13 09:50:34 +0100

    [ADD] MoodMapper

---
+++ src/main/java/be/intecbrussel/moodtracker/models/mappers/MoodMapper.java
@@ -0,0 +1,26 @@
+package be.intecbrussel.moodtracker.models.mappers;
+
+import be.intecbrussel.moodtracker.models.Mood;
+import be.intecbrussel.moodtracker.models.dtos.MoodDTO;
+
+public class MoodMapper {
+
+    public static MoodDTO mapMoodToMoodDTO(Mood mood) {
+
+        return new MoodDTO(
+                mood.getMoodID(),
+                mood.getEmotion(),
+                mood.getRating(),
+                mood.getDescription());
+    }
+
+    public static Mood mapMoodDTOToMood(MoodDTO moodDTO) {
+
+        return new Mood(
+                moodDTO.getMoodID(),
+                moodDTO.getEmotion(),
+                moodDTO.getRating(),
+                moodDTO.getDescription());
+    }
+
+}
