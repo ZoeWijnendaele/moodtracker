f4a565dac9426872c19c7db3b7468c1ac942b4be
zoe <zoewijnendaele@gmail.com>
2024-02-13 10:01:48 +0100

    [ADD] MoodRepository

--- src/main/java/be/intecbrussel/moodtracker/repositories/MoodRepository.java
+++ src/main/java/be/intecbrussel/moodtracker/repositories/MoodRepository.java
@@ -4,4 +4,5 @@ import be.intecbrussel.moodtracker.models.Mood;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface MoodRepository extends JpaRepository<Mood, Long> {
+
 }
