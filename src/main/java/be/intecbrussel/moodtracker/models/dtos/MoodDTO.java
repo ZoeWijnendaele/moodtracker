5327e82f3d7d9c7574322c3171b0dcc8a63a1ee8
zoe <zoewijnendaele@gmail.com>
2024-02-13 09:48:36 +0100

    [ADD] MoodDTO

--- src/main/java/be/intecbrussel/moodtracker/models/dtos/MoodDTO.java
+++ src/main/java/be/intecbrussel/moodtracker/models/dtos/MoodDTO.java
@@ -1,4 +1,59 @@
 package be.intecbrussel.moodtracker.models.dtos;
 
+import be.intecbrussel.moodtracker.models.enums.Emotion;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.validation.constraints.NotEmpty;
+
 public class MoodDTO {
+
+    private Long moodID;
+    @NotEmpty(message = "Please provide an emotion")
+    @Enumerated(EnumType.STRING)
+    private Emotion emotion;
+    @NotEmpty(message = "Please provide a rating")
+    private int rating;
+    private String description;
+
+    public MoodDTO() { }
+
+    public MoodDTO(Long moodID, Emotion emotion, int rating, String description) {
+        this.moodID = moodID;
+        this.emotion = emotion;
+        this.rating = rating;
+        this.description = description;
+    }
+
+    public Long getMoodID() {
+        return moodID;
+    }
+
+    public void setMoodID(Long moodID) {
+        this.moodID = moodID;
+    }
+
+    public Emotion getEmotion() {
+        return emotion;
+    }
+
+    public void setEmotion(Emotion emotion) {
+        this.emotion = emotion;
+    }
+
+    public int getRating() {
+        return rating;
+    }
+
+    public void setRating(int rating) {
+        this.rating = rating;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
 }
