30f331b5d5660cfebe67da0e1489147170c18790
zoe <zoewijnendaele@gmail.com>
2024-02-14 11:20:04 +0100

    [WIP] test methods crud

--- src/test/java/be/intecbrussel/moodtracker/services/impl/MoodServiceImplTest.java
+++ src/test/java/be/intecbrussel/moodtracker/services/impl/MoodServiceImplTest.java
@@ -1,8 +1,8 @@
 package be.intecbrussel.moodtracker.services.impl;
 
+import be.intecbrussel.moodtracker.exceptions.MergeFailureException;
 import be.intecbrussel.moodtracker.exceptions.PresentInDatabaseException;
 import be.intecbrussel.moodtracker.exceptions.ResourceNotFoundException;
-import be.intecbrussel.moodtracker.models.Client;
 import be.intecbrussel.moodtracker.models.Mood;
 import be.intecbrussel.moodtracker.models.dtos.MoodDTO;
 import be.intecbrussel.moodtracker.models.enums.Emotion;
@@ -16,18 +16,16 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.http.HttpStatus;
 
 import java.util.List;
 import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.anyInt;
-import static org.mockito.BDDMockito.given;
-import static org.mockito.BDDMockito.willAnswer;
-import static org.mockito.Mockito.times;
-import static org.mockito.Mockito.verify;
+import static org.mockito.ArgumentMatchers.*;
+import static org.mockito.BDDMockito.*;
 
 @ExtendWith(MockitoExtension.class)
 class MoodServiceImplTest {
@@ -107,7 +105,7 @@ class MoodServiceImplTest {
     }
 
     @Test
-    public void givenMoodDTO_whenGetALLMoods_thenReturnListOfMoodObjects() {
+    public void givenMoodDTO_whenGetAllMoods_thenReturnListOfMoodObjects() {
         Mood mood1 = new Mood(1L, Emotion.AMUSED, 5, "I am happy");
         Mood mood2 = new Mood(2L, Emotion.ANGRY, 2);
         List<Mood> moods = List.of(mood1, mood2);
@@ -132,6 +130,76 @@ class MoodServiceImplTest {
     }
 
     @Test
-    public void givenMoodDTO_whenUpdateMood_thenReturnUpdatedMood() {
+    public void givenMoodDTO_whenUpdateMood_thenReturnUpdatedMoodDTO() {
+
+        given(moodRepository.findById(anyLong())).willReturn(Optional.of(mood));
+        given(moodRepository.save(any(Mood.class))).willAnswer(invocation -> invocation.getArgument(0));
+
+        Mood updatedMood = moodService.updateMood(moodDTO, mood.getMoodID());
+
+        assertThat(updatedMood).isNotNull();
+        assertThat(updatedMood.getMoodID()).isEqualTo(moodDTO.getMoodID());
+        assertThat(updatedMood.getEmotion()).isEqualTo(moodDTO.getEmotion());
+        assertThat(updatedMood.getRating()).isEqualTo(moodDTO.getRating());
+        assertThat(updatedMood.getDescription()).isEqualTo(moodDTO.getDescription());
+
+        verify(moodMergerService, times(1)).mergeMood(mood.getMoodID(), moodDTO);
+        verify(moodRepository, times(1)).findById(mood.getMoodID());
+        verify(moodRepository, times(1)).save(mood);
+    }
+
+    @Test
+    public void givenInvalidMoodID_whenUpdateMood_thenReturnResourceNotFoundException() {
+        given(moodRepository.findById(anyLong())).willReturn(Optional.empty());
+
+        assertThrows(ResourceNotFoundException.class, () ->
+                moodService.updateMood(moodDTO, mood.getMoodID()));
+
+        verify(moodRepository, times(1)).findById(mood.getMoodID());
+        verify(moodMergerService, never()).mergeMood(mood.getMoodID(), moodDTO);
+        verify(moodRepository, never()).save(mood);
+    }
+
+    @Test
+    public void givenMergeFailureMoodDTO_whenUpdateMood_thenReturnMergeFailureException() {
+        given(moodRepository.findById(anyLong())).willReturn(Optional.of(mood));
+
+        doThrow(new MergeFailureException("Mood", "id", String.valueOf(mood.getMoodID())))
+                .when(moodMergerService).mergeMood(mood.getMoodID(), moodDTO);
+
+        MergeFailureException mergeFailureException =
+                assertThrows(MergeFailureException.class, () ->
+                        moodService.updateMood(moodDTO, mood.getMoodID()));
+
+        assertThat(mergeFailureException.getMessage())
+                .isEqualTo("Mood with id: '1' not able to merge");
+
+        verify(moodRepository, times(1)).findById(mood.getMoodID());
+        verify(moodMergerService, times(1)).mergeMood(mood.getMoodID(), moodDTO);
+        verify(moodRepository, never()).save(mood);
+    }
+
+    @Test
+    public void givenMoodDTO_whenDeleteMood_thenReturnVoid() {
+        willDoNothing().given(moodRepository).deleteById(mood.getMoodID());
+        moodService.deleteMood(mood.getMoodID());
+
+        verify(moodRepository, times(1)).deleteById(mood.getMoodID());
+    }
+
+    @Test
+    public void givenInvalidMoodID_whenDeleteMood_thenReturnResourceNotFoundException() {
+        Long invalidMoodID = -1L;
+        willThrow(new EmptyResultDataAccessException(0)).given(moodRepository).deleteById(invalidMoodID);
+
+        ResourceNotFoundException resourceNotFoundException =
+                assertThrows(ResourceNotFoundException.class, () ->
+                        moodService.deleteMood(invalidMoodID));
+
+        assertThat(resourceNotFoundException.getMessage())
+                .isEqualTo("Mood with id: '-1' not found in database");
+        assertThat(resourceNotFoundException.getStatus()).isEqualTo(HttpStatus.NOT_FOUND);
+
+        verify(moodRepository, times(1)).deleteById(invalidMoodID);
     }
 }
