2e01ed81d9c799d32c1c7066b9c89b702612b70f
zoe <zoewijnendaele@gmail.com>
2024-02-19 13:54:36 +0100

    [WIP] updated repo, for calendar

--- src/main/java/be/intecbrussel/moodtracker/services/MoodService.java
+++ src/main/java/be/intecbrussel/moodtracker/services/MoodService.java
@@ -1,8 +1,12 @@
 package be.intecbrussel.moodtracker.services;
 
+import be.intecbrussel.moodtracker.models.Client;
 import be.intecbrussel.moodtracker.models.Mood;
 import be.intecbrussel.moodtracker.models.dtos.MoodDTO;
+import be.intecbrussel.moodtracker.models.enums.Emotion;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 
@@ -11,7 +15,10 @@ public interface MoodService {
     void addMood(MoodDTO moodDTO);
     Optional<MoodDTO> getMoodById(Long id);
     List<MoodDTO> getAllMoods();
+    List<Mood> getMoodsForDate(Client client, LocalDateTime dateTime);
     Mood updateMood(MoodDTO moodDTO, Long id);
+    Emotion averageEmotion(List<Mood> moods);
     void deleteMood(Long id);
+
 
 }
