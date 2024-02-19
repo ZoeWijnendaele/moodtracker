0361cd71032709453dea97b55ab555497a22cce5
zoe <zoewijnendaele@gmail.com>
2024-02-19 13:54:14 +0100

    [WIP] updated repo, for calendar

--- src/main/java/be/intecbrussel/moodtracker/repositories/MoodRepository.java
+++ src/main/java/be/intecbrussel/moodtracker/repositories/MoodRepository.java
@@ -1,13 +1,18 @@
 package be.intecbrussel.moodtracker.repositories;
 
+import be.intecbrussel.moodtracker.models.Client;
 import be.intecbrussel.moodtracker.models.Mood;
 import be.intecbrussel.moodtracker.models.enums.Emotion;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.List;
 import java.util.Optional;
 
 public interface MoodRepository extends JpaRepository<Mood, Long> {
 
     Optional<Mood> findByEmotionAndRatingAndDescription(Emotion emotion, int rating, String description);
+    List<Mood> findByClientAndDateTime(Client client, LocalDateTime dateTime);
 
 }
